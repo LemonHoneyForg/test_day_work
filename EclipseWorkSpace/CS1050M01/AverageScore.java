2d0982900375a67bf74d55bf631d8fbc1144f58a
LemonHoneyForg <loridguerrer0@gmail.com>
2026-02-02 13:05:54 -0700

    Update AverageScore.java

--- EclipseWorkSpace/CS1050M01/AverageScore.java
+++ EclipseWorkSpace/CS1050M01/AverageScore.java
@@ -6,8 +6,8 @@ public class AverageScore
 		int score1 = 80;
 		int score2 = 97;
 		int score3 = 85;
-		double total = score1 + score2 + score3;
-		double average = total / 3;
+		int total = score1 + score2 + score3;
+		double average = total / 3.0;
 		System.out.println("Total: " + total);
 		System.out.println("Average " + average);
 	}// end of main
