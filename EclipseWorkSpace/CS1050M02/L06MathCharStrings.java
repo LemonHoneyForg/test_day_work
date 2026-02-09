ffdd86bbfb2986d13ad3601cc30260c069ab4305
LemonHoneyForg <loridguerrer0@gmail.com>
2026-02-09 13:38:18 -0700

    Update L06MathCharStrings.java

--- EclipseWorkSpace/CS1050M02/L06MathCharStrings.java
+++ EclipseWorkSpace/CS1050M02/L06MathCharStrings.java
@@ -1,20 +1,31 @@
-/**
- * 
- */
 
 /**
  * 
  */
+import java.util.Scanner;
+
 public class L06MathCharStrings
 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String firstName;
 
-	}
+		System.out.println("Enter first name");
 
-}
+		firstName = input.next();
+
+		System.out.println("First name " + firstName);
+
+		System.out.println("Enter the initial of your last name");
+
+		char lastInitial = input.next().charAt(0);
+
+		String summary = "Hello " + firstName + " " + lastInitial;
+
+		System.out.println(summary);
+		input.close();
+	} // end of main
+
+}// end of class
