a30f077d16f0344a5067d144f06c64898ea47bcd
LemonHoneyForg <loridguerrer0@gmail.com>
2026-02-17 12:50:06 -0700

    Create ConcertTicketPrices.java

---
+++ EclipseWorkSpace/CS1050M02/ConcertTicketPrices.java
@@ -0,0 +1,79 @@
+
+/**
+ * 
+ */
+
+import java.util.Scanner;
+
+public class ConcertTicketPrices
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int rowOne = 450;
+		int rowTwo = 300;
+		int rowThree = 200;
+
+		char sectionF = 'F';
+		char sectionC = 'C';
+		char sectionS = 'S';
+		char sectionU = 'U';
+		char userSection;
+		int userRow;
+		System.out.println(" For your ticket please enter the letter of the section you would like from F,C,S or U");
+
+		userSection = input.next().charAt(0);
+		Character.toUpperCase(userSection);
+
+		if (userSection == sectionF)
+		{
+			System.out.println("understood Section F what row from 1 through 60?");
+			userRow = input.nextInt();
+
+		} else if (userSection == sectionC)
+		{
+
+			System.out.println("understood Section C what row from 1 through 60?");
+			userRow = input.nextInt();
+		} else if (userSection == sectionS)
+		{
+			System.out.println("understood Section S what row from 1 through 60?");
+			userRow = input.nextInt();
+
+		} else if (userSection == sectionU)
+		{
+			System.out.println("understood Section U what row from 1 through 60?");
+			userRow = input.nextInt();
+
+		} else
+		{
+			System.out.println("Invalid row,program ending");
+		}
+
+		if (userRow <= 1)
+		{
+			System.out.println("Invalid row,  program ending");
+		} else if (userRow <= 15)
+		{
+
+			System.out.println("understood your ticket will cost $" + rowOne);
+		} else if (userRow <= 30)
+		{
+
+			System.out.println("understood your ticket will cost $" + rowTwo);
+		} else if (userRow <= 60)
+		{
+
+			System.out.println("understood your ticket will cost $" + rowThree);
+		} else
+		{
+			System.out.println("Invalid row,  program ending");
+		}
+		input.close();
+	}// end of main
+
+}// end of class
