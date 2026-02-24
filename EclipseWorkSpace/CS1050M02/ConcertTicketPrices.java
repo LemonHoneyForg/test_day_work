5fc15bc79ec3b6fae3a3a388927fbe06ff373c6c
LemonHoneyForg <loridguerrer0@gmail.com>
2026-02-23 22:04:04 -0700

    final update

--- EclipseWorkSpace/CS1050M02/ConcertTicketPrices.java
+++ EclipseWorkSpace/CS1050M02/ConcertTicketPrices.java
@@ -8,12 +8,16 @@ import java.util.Scanner;
 public class ConcertTicketPrices
 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
+		/*
+		 * Establishing all variables at the start of the program to ensure that
+		 * everything is organized in one place where it makes sense meaning that the
+		 * details on the cost of the three rows is organized at the top of the program
+		 * as well as the applicable sections that are available our initialized before
+		 * we get into the if statements
+		 */
 		int rowOne = 450;
 		int rowTwo = 300;
 		int rowThree = 200;
@@ -24,17 +28,24 @@ public class ConcertTicketPrices
 		char sectionU = 'U';
 		char userSection;
 		int userRow;
+
+		/*
+		 * Takes in the first user input regarding the section and capitalizes it so
+		 * that we can case match with the section names that we established beforehand
+		 * properly
+		 */
 		System.out.println(" For your ticket please enter the letter of the section you would like from F,C,S or U");
 
 		userSection = input.next().charAt(0);
 		Character.toUpperCase(userSection);
-
+		// checks input to established user sections and if matchs moves on to row
+		// selection
 		if ((userSection == sectionF) || (userSection == sectionC) || (userSection == sectionS)
 				|| (userSection == sectionU))
 		{
 			System.out.println("understood Section " + userSection + " what row from 1 through 60?");
 			userRow = input.nextInt();
-
+			// Determines cost based on row and tells the user based on they're choice
 			if (userRow < 1)
 			{
 				System.out.println("Invalid row,  program ending");
@@ -50,12 +61,12 @@ public class ConcertTicketPrices
 			{
 
 				System.out.println("understood your ticket will cost $" + rowThree);
-			} else
+			} else // ends program if user input is out of range (1-60)
 			{
 				System.out.println("Invalid row,  program ending");
 			}
 
-		} else
+		} else // ends program if input doesn't match sections avalible
 		{
 			System.out.println("Invalid section,program ending");
 		}
