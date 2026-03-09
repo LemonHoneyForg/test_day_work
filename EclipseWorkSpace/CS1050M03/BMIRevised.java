6e9a9778930f13145fc43f1ccb0e4d67365ab59b
LemonHoneyForg <loridguerrer0@gmail.com>
2026-03-09 15:05:36 -0600

    loop for new bmi user works as intended

--- EclipseWorkSpace/CS1050M03/BMIRevised.java
+++ EclipseWorkSpace/CS1050M03/BMIRevised.java
@@ -20,35 +20,45 @@ public class BMIRevised
 		// The user's height
 		double height;
 		// The user's BMI
+		boolean runBmiAgain = false;
 
+		char userInput;
 		// Create a Scanner object for scanning input from keyboard
 		Scanner keyboardInput = new Scanner(System.in);
-
-		// Tell the user what the program will do.
-		System.out.println("This program will calculate your body mass index, or BMI.");
-
-		// Get the user's weight.
-		System.out.print("Enter your weight, in pounds: ");
-		// ADD CODE to input next double for weight
-		weight = keyboardInput.nextDouble();
-		// Get the user's height.
-		System.out.print("Enter your height, in inches: ");
-		// ADD CODE input next double for height
-		height = keyboardInput.nextDouble();
-
-		boolean validBmi = realBmi(weight, height);
-
-		if (validBmi == true)
+		do
 		{
-			double endBmi = printBmi(weight, height);
+			// Tell the user what the program will do.
+			System.out.println("This program will calculate your body mass index, or BMI.");
 
-			System.out.printf("\twith a Height of " + height + " and Weight of " + weight + " athlete’s BMI is %.2f",
-					endBmi);
+			// Get the user's weight.
+			System.out.print("Enter your weight, in pounds: ");
+			// ADD CODE to input next double for weight
+			weight = keyboardInput.nextDouble();
+			// Get the user's height.
+			System.out.print("Enter your height, in inches: ");
+			// ADD CODE input next double for height
+			height = keyboardInput.nextDouble();
 
-		} else
-		{
-			System.out.println("sorry user weight or height can not be zero ending program");
-		}
+			boolean validBmi = realBmi(weight, height);
+
+			if (validBmi == true)
+			{
+				double endBmi = printBmi(weight, height);
+
+				System.out.printf(
+						"\twith a Height of " + height + " and Weight of " + weight + " athlete’s BMI is %.2f", endBmi);
+				System.out.println();
+				System.out.println("would you like to enter another athlete’s data for BMI");
+				userInput = keyboardInput.next().charAt(0);
+				char capitalChar = Character.toUpperCase(userInput);
+				runBmiAgain = runAgain(capitalChar);
+			} else
+			{
+				System.out.println("sorry user weight or height can not be zero ending program");
+			}
+
+		} while (runBmiAgain == true);
+		System.out.println("understood ending program");
 		keyboardInput.close();
 	}// end of main
 
@@ -73,6 +83,20 @@ public class BMIRevised
 			bmiValue = true;
 		}
 		return bmiValue;
+	}
+
+	public static boolean runAgain(char userAnswer)
+	{
+
+		boolean programRepeat = false;
+		if ((userAnswer == 'Y'))
+		{
+			programRepeat = true;
+		} else
+		{
+			programRepeat = false;
+		}
+		return programRepeat;
 	}
 
 }// end of class
